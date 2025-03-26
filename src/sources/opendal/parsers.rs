99ee09fce804fdb00192b150ae66cf66d92c1ae1
David Bernard <david.bernard.31@gmail.com>
2025-03-26 19:26:21 +0100

    feat: add a parser for jsonl

--- src/sources/opendal/parsers.rs
+++ src/sources/opendal/parsers.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, io::BufRead};
 
 use crate::{
     errors::{IntoDiagnostic, Result},
@@ -14,21 +14,24 @@ use super::Resource;
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub(crate) enum Config {
-    #[serde(alias = "json")]
-    Json,
-    #[serde(alias = "metadata")]
-    Metadata,
     #[serde(alias = "csv_row")]
     CsvRow,
+    #[serde(alias = "json")]
+    Json,
+    #[serde(alias = "jsonl")]
+    Jsonl,
+    #[serde(alias = "metadata")]
+    Metadata,
 }
 
 impl Config {
     #[allow(clippy::unnecessary_wraps)]
     pub(crate) fn make_parser(&self, next: EventSourcePipe) -> Result<ParserEnum> {
         let out = match self {
-            Config::Json => JsonParser::new(next).into(),
-            Config::Metadata => MetadataParser::new(next).into(),
             Config::CsvRow => CsvRowParser::new(next).into(),
+            Config::Json => JsonParser::new(next).into(),
+            Config::Jsonl => JsonlParser::new(next).into(),
+            Config::Metadata => MetadataParser::new(next).into(),
         };
         Ok(out)
     }
@@ -37,9 +40,10 @@ impl Config {
 #[enum_dispatch]
 #[allow(clippy::enum_variant_names)]
 pub(crate) enum ParserEnum {
-    JsonParser,
-    MetadataParser,
     CsvRowParser,
+    JsonParser,
+    JsonlParser,
+    MetadataParser,
 }
 
 #[enum_dispatch(ParserEnum)]
@@ -82,6 +86,34 @@ impl Parser for JsonParser {
         let body: serde_json::Value = serde_json::from_reader(bytes.reader()).into_diagnostic()?;
         let event = EventSource { metadata, body, ..Default::default() };
         self.next.send(event)
+    }
+}
+
+pub(crate) struct JsonlParser {
+    next: EventSourcePipe,
+}
+
+impl JsonlParser {
+    fn new(next: EventSourcePipe) -> Self {
+        Self { next }
+    }
+}
+
+impl Parser for JsonlParser {
+    async fn parse(&mut self, op: &Operator, resource: &Resource) -> Result<()> {
+        let bytes = op.read(resource.path()).await.into_diagnostic()?;
+        let metadata = resource.as_json_metadata();
+        let mut reader = bytes.reader();
+        let mut buf = String::new();
+        while reader.read_line(&mut buf).into_diagnostic()? > 0 {
+            if buf.is_empty() {
+                continue;
+            }
+            let body: serde_json::Value = serde_json::from_str(&buf).into_diagnostic()?;
+            let event = EventSource { metadata: metadata.clone(), body, ..Default::default() };
+            self.next.send(event)?;
+        }
+        Ok(())
     }
 }
 
